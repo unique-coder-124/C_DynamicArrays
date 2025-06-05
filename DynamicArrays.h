b9fe552cf37500882e45f8aa91d0c6f0535fab49
Michael Kershaw <michaelkershaw2006@gmail.com>
2025-06-05 21:50:14 +1000

    fixed a few things that i don't remember

--- DynamicArrays.h
+++ DynamicArrays.h
@@ -4,24 +4,25 @@
 #include <stdlib.h>
 #include <stddef.h>
 
-#define init_NamedDynamicArray_ofType(name, type)                                       \
-  typedef struct {                                                                      \
-    type* items;                                                                        \
-    size_t count;                                                                       \
-    size_t capacity;                                                                    \
-  } name
+#define DynamicArray(type)      \
+  struct {                      \
+    type* items;                \
+    size_t count;               \
+    size_t capacity;            \
+  }
 
-#define DynamicArray_Append(arr, item) do {                                             \
-  if (arr.count > arr.capacity) {                                                       \
-    if (arr.capacity == 0) {                                                            \
-      arr.capacity = 128;                                                               \
-    } else {                                                                            \
-      arr.capacity *= 2;                                                                \
-    }                                                                                   \
-    arr.items = realloc(arr.items, arr.capacity*sizeof(*arr.items));                    \
-                                                                                        \
-  }                                                                                     \
-  arr.items[arr.count++] = item;                                                        \
-} while (0)
+#define DynamicArray_Append(_ARR, _ITEM) ({                                 \
+  if (_ARR.count >= _ARR.capacity) {                                          \
+    if (_ARR.capacity == 0) {                                                 \
+      _ARR.capacity = 128;                                                    \
+    } else {                                                                  \
+      _ARR.capacity *= 2;                                                     \
+    }                                                                         \
+    _ARR.items = realloc(_ARR.items, _ARR.capacity*sizeof(*_ARR.items));      \
+  }                                                                           \
+  _ARR.items[_ARR.count++] = _ITEM;                                           \
+})
+
+#define DynamicArray_remove(_ARR, _IDX)
 
 #endif
--- main.c
+++ main.c
@@ -1,19 +1,23 @@
 #include <stdio.h>
 #include "DynamicArrays.h"
 
-init_NamedDynamicArray_ofType(IntArray, int);
+typedef DynamicArray(int) IntArray;
 
 int main(int argc, char **argv) {
-  IntArray test;
+  IntArray test = {0};
 
   for (int i = 0; i < 100; i++) {
     DynamicArray_Append(test, i);
   }
 
-  for (int i = 0; i < test.count; i++) {
-    printf("%d\n", test.items[i]);
-  }
+  printf("%zd\n", test.count);
 
-  free(test.items);
+  printf("[");
+  for (int i = 0; i < test.count - 1; i++) {
+    printf("%d, ", test.items[i]);
+  }
+  printf("%d]", test.items[test.count - 1]);
+
+  // free(test.items);
   return 0;
 }
