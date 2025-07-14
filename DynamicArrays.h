71e2e72b84f09f6a33c973bcf51126e12c0d40b6
MichaelKershaw <MichaelKershaw2006@gmail.com>
2025-07-14 21:29:21 +1000

    most things done including insert and append. not sure if i want to bother adding anything else. accidentally used this commit message in the wrong repo before

--- DynamicArrays.h
+++ DynamicArrays.h
@@ -1,28 +1,105 @@
-#ifndef DYNAMIC_ARRAY_H
-#define DYNAMIC_ARRAY_H
-
 #include <stdlib.h>
 #include <stddef.h>
 
-#define DynamicArray(type)      \
-  struct {                      \
-    type* items;                \
-    size_t count;               \
-    size_t capacity;            \
+#ifndef DYNAMIC_ARRAY_H
+#define DYNAMIC_ARRAY_H
+
+#if defined(__clang__)
+  #define DIAGNOSTIC_PUSH    _Pragma("clang diagnostic push")
+  #define DIAGNOSTIC_IGNORE_FORMAT  _Pragma("clang diagnostic ignored \"-Wformat\"")
+  #define DIAGNOSTIC_IGNORE_UNUSED  _Pragma("clang diagnostic ignored \"-Wunused-value\"")
+  #define DIAGNOSTIC_POP     _Pragma("clang diagnostic pop")
+#elif defined(__GNUC__)
+  #define DIAGNOSTIC_PUSH    _Pragma("GCC diagnostic push")
+  #define DIAGNOSTIC_IGNORE_FORMAT  _Pragma("GCC diagnostic ignored \"-Wformat\"")
+  #define DIAGNOSTIC_IGNORE_UNUSED  _Pragma("GCC diagnostic ignored \"-Wunused-value\"")
+  #define DIAGNOSTIC_POP     _Pragma("GCC diagnostic pop")
+#else
+  #define DIAGNOSTIC_PUSH
+  #define DIAGNOSTIC_IGNORE_FORMAT
+  #define DIAGNOSTIC_IGNORE_UNUSED
+  #define DIAGNOSTIC_POP
+#endif
+
+#define DA(type)          \
+  struct {                \
+    type* items;          \
+    size_t count;         \
+    size_t capacity;      \
   }
 
-#define DynamicArray_Append(_ARR, _ITEM) ({                                 \
+#define DA_Append(_ARR, _VAL) ({                                              \
+  DIAGNOSTIC_PUSH                                                             \
+  DIAGNOSTIC_IGNORE_UNUSED                                                    \
+  int _RESULT = 1;                                                            \
   if (_ARR.count >= _ARR.capacity) {                                          \
     if (_ARR.capacity == 0) {                                                 \
       _ARR.capacity = 128;                                                    \
     } else {                                                                  \
       _ARR.capacity *= 2;                                                     \
     }                                                                         \
+    void* _OLDPTR = _ARR.items;                                               \
     _ARR.items = realloc(_ARR.items, _ARR.capacity*sizeof(*_ARR.items));      \
+    if (!_ARR.items) {                                                        \
+      _RESULT = 0;                                                            \
+      _ARR.items = _OLDPTR;                                                   \
+    } else {                                                                  \
+      _ARR.items[_ARR.count++] = _VAL;                                        \
+    }                                                                         \
+  } else {                                                                    \
+    _ARR.items[_ARR.count++] = _VAL;                                          \
   }                                                                           \
-  _ARR.items[_ARR.count++] = _ITEM;                                           \
+  _RESULT;                                                                    \
+  DIAGNOSTIC_POP                                                              \
 })
 
-#define DynamicArray_remove(_ARR, _IDX)
+#define DA_Remove(_ARR, _IDX) ({                          \
+  DIAGNOSTIC_PUSH                                         \
+  DIAGNOSTIC_IGNORE_UNUSED                                \
+  int _RESULT = 1;                                        \
+  if (_IDX < 0 || _IDX >= _ARR.count) {                   \
+    _RESULT = 0;                                          \
+  } else {                                                \
+    for (int _I = _IDX; _I < _ARR.count - 1; _I++) {      \
+      _ARR.items[_I] = _ARR.items[_I + 1];                \
+    }                                                     \
+    _ARR.count--;                                         \
+  }                                                       \
+  _RESULT;                                                \
+  DIAGNOSTIC_POP                                          \
+})
+
+#define DA_Insert(_ARR, _IDX, _VAL) ({                                        \
+  DIAGNOSTIC_PUSH                                                             \
+  DIAGNOSTIC_IGNORE_UNUSED                                                    \
+  int _RESULT = 1;                                                            \
+  if (_ARR.count >= _ARR.capacity) {                                          \
+    if (_ARR.capacity == 0) {                                                 \
+      _ARR.capacity = 128;                                                    \
+    } else {                                                                  \
+      _ARR.capacity *= 2;                                                     \
+    }                                                                         \
+    void* _OLDPTR = _ARR.items;                                               \
+    _ARR.items = realloc(_ARR.items, _ARR.capacity*sizeof(*_ARR.items));      \
+    if (!_ARR.items) {                                                        \
+      _RESULT = 0;                                                            \
+      _ARR.items = _OLDPTR;                                                   \
+    } else {                                                                  \
+      _ARR.count++;                                                           \
+      for (int _I = _ARR.count; _I > _IDX; _I--) {                            \
+        _ARR.items[_I] = _ARR.items[_I - 1];                                  \
+      }                                                                       \
+      _ARR.items[_IDX] = _VAL;                                                \
+    }                                                                         \
+  } else {                                                                    \
+    _ARR.count++;                                                             \
+    for (int _I = _ARR.count; _I > _IDX; _I--) {                              \
+      _ARR.items[_I] = _ARR.items[_I - 1];                                    \
+    }                                                                         \
+    _ARR.items[_IDX] = _VAL;                                                  \
+  }                                                                           \
+  _RESULT;                                                                    \
+  DIAGNOSTIC_POP                                                              \
+})
 
 #endif
--- main.c
+++ main.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include "DynamicArrays.h"
 
-typedef DynamicArray(int) IntArray;
+typedef DA(int) IntArray;
 
 int main(int argc, char **argv) {
   IntArray test = {0};
 
   for (int i = 0; i < 100; i++) {
-    DynamicArray_Append(test, i);
+    DA_Append(test, i);
   }
 
   printf("%zd\n", test.count);
@@ -17,7 +17,34 @@ int main(int argc, char **argv) {
     printf("%d, ", test.items[i]);
   }
   printf("%d]", test.items[test.count - 1]);
+  printf("\n");
 
-  // free(test.items);
+  for (int i = 0; i < 50; i++) {
+    DA_Remove(test, i);
+  }
+
+  printf("%zd\n", test.count);
+
+  printf("[");
+  for (int i = 0; i < test.count - 1; i++) {
+    printf("%d, ", test.items[i]);
+  }
+  printf("%d]", test.items[test.count - 1]);
+  printf("\n");
+
+  for (int i = 0; i < 50; i++) {
+    DA_Insert(test, 2*i, 2*i);
+  }
+
+  printf("%zd\n", test.count);
+
+  printf("[");
+  for (int i = 0; i < test.count - 1; i++) {
+    printf("%d, ", test.items[i]);
+  }
+  printf("%d]", test.items[test.count - 1]);
+  printf("\n");
+
+  free(test.items);
   return 0;
 }
